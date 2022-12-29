00b269d8515bc80791cdbe2e6dfec826dfbe735a
bdeborchels <b.elices.garcia@gmail.com>
2022-12-30 00:40:45 +0100

    ReverseString exercise donde

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    //Primero lo dividimos cada palabra en un array
+    //Damos la vuelta al array
+    //Finalmente volvemos a juntar las letras de las palabras
 
+    let splitString = string.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+
+    return joinArray;
 };
-
+reverseString('hello');
+reverseString('hello there');
+reverseString('123! abc!');
+reverseString('');
 // Do not edit below this line
 module.exports = reverseString;
