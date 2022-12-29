c869114e25e9708c4b9bbe5f9cabe717a83fa7ec
bdeborchels <b.elices.garcia@gmail.com>
2022-12-29 21:09:53 +0100

    repeatString tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+if (times < 0) return 'ERROR';
 
-};
+    let string = '';
+    for (let i = 0; i < times; i++) {
+      string += word;
+    }
+    return string;
+  };
+
+  repeatString('hey',3);
+  repeatString('hey',10);
+  repeatString('hey',1);
+  repeatString('hey',0);
+  repeatString('hey',10).match(/((hey))/g).length;
+  repeatString('',3);
 
 // Do not edit below this line
 module.exports = repeatString;
