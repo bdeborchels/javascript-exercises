d41b339106ede54c272b6f2907b913f1393939ad
bdeborchels <b.elices.garcia@gmail.com>
2022-12-30 18:58:29 +0100

    06 exercise done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    var isLeap = false;
+
+    if((year%4 ==0) && (year%100!=0 || year%400==0)) {
+        isLeap = true;
+    }
+    return isLeap;
 
 };
+
+leapYears(1996);
+leapYears(1997);
+leapYears(34992);
+leapYears(1900);
+leapYears(1600);
+leapYears(700);
+
 
 // Do not edit below this line
 module.exports = leapYears;
