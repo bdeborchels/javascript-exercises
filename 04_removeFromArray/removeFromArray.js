bb0b0e759529c895b76af5c8f7c9968a5889090d
bdeborchels <b.elices.garcia@gmail.com>
2022-12-30 15:55:00 +0100

    04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function (...args) {
+    // the very first item in our list of arguments is the array, we pull it out with args[0]
+    const array = args[0];
+    const newArray = [];
+   
+    array.forEach((item) => {
+      // push every element into the new array
+      // UNLESS it is included in the function arguments
+      // so we create a new array with every item, except those that should be removed
+      if (!args.includes(item)) {
+        newArray.push(item);
+      }
+    });
+    return newArray;
+  };
 
-};
+  removeFromArray([1, 2, 3, 4], 3);
+  removeFromArray([1, 2, 3, 4], 3, 2);
+  removeFromArray([1, 2, 3, 4], 7, "tacos");
+  removeFromArray([1, 2, 3, 4], 7, 2);
+  removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
+  removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+  removeFromArray([1, 2, 3], "1", 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
