c172967e2fb537b2f2f288b4dc17ffc1432005bb
bdeborchels <b.elices.garcia@gmail.com>
2023-01-16 23:30:50 +0100

    fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function(num) {
+    num = num * 1;
+    if(num <0){
+        return "OOPS"
+    }
+        if (num <= 1) return num;
+    
+        return fibonacci(num-1) + fibonacci(num-2);
+}
+fibonacci(4);
+fibonacci(6);
+fibonacci(10);
+fibonacci(15);
+fibonacci(25);
+fibonacci(-25);
+fibonacci("1");
+fibonacci("2");
+fibonacci("8");
 // Do not edit below this line
 module.exports = fibonacci;
