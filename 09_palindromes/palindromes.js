1daaa8c3853bd1294d08b67724030c2af1b6c55a
bdeborchels <b.elices.garcia@gmail.com>
2023-01-16 23:02:06 +0100

    palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    //pasamos todo a minusculas y quitamos caracteres no deseados (espacios, comas, puntos..)
+    //dividimos el string, le damos la vuelta y lo volvemos a juntar
+    //comparamos si son iguales
+
+    var re = /[\W_]/g;
+    var lowRegStr = string.toLowerCase().replace(re, '');
+    var reverseStr = lowRegStr.split('').reverse().join(''); 
+
+    return reverseStr == lowRegStr;
 
 };
+palindromes('racecar');
+palindromes('racecar!');
+palindromes('Racecar!');
+palindromes('A car, a man, a maraca.');
+palindromes('Animal loots foliated detail of stool lamina.');
+palindromes('ZZZZ car, a man, a maracaz.');
 
 // Do not edit below this line
 module.exports = palindromes;
