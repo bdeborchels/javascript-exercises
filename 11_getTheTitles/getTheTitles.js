2af8a6765181fb190c54c4e247f51ace2be23b29
bdeborchels <b.elices.garcia@gmail.com>
2023-01-16 23:45:33 +0100

    array of titles done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,20 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+const getTheTitles = function(objArray) {
+    let result = objArray.map(a => a.title);
+
+    return result;
 
 };
-
+getTheTitles(books);
 // Do not edit below this line
 module.exports = getTheTitles;
