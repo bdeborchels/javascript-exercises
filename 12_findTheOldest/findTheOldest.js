bb6378837952b2e26a4f51d82a501bc1527e45f5
bdeborchels <b.elices.garcia@gmail.com>
2023-01-18 13:28:31 +0100

    findTheOldest ex done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const date = new Date();
+    let year = date.getFullYear();
+    let oldest = {};
+    let oldestAge = 0;
+  for (let i = 0; i < people.length; i++) {
+    let age = people[i].yearOfDeath - people[i].yearOfBirth;
 
+    if(people[i].yearOfDeath == undefined){
+        age = year - people[i].yearOfBirth;
+    }
+    if( age >= oldestAge) {
+       oldestAge = age;
+       oldest = people[i];
+    }
+  }
+
+  return oldest;
 };
 
 // Do not edit below this line
